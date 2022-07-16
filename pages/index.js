4869683003933c0a4567b3f955aa1f36eaeea9d1
unknown <atiqit52@gmail.com>
2022-07-16 22:52:31 +0600

    slider added

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import Featured from '../components/Featured'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      {/* <Navbar /> */}
+    
       <Head>
         <title>Pizza Restaurant in Dhaka</title>
         <meta name="description" content="Best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-homepage
-      {/* <Footer /> */}
+    <Featured />
+   
     </div>
   )
 }
